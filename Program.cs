9cc4f553c00cc0014430f9d53bc731ee3e26a6d4
Redi <redicurrii@yahoo.com>
2026-01-07 08:37:36 +0100

    Test

--- Program.cs
+++ Program.cs
@@ -3,12 +3,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        MatrixOrient matrixOrient = new MatrixOrient();
-        matrixOrient.matrix(new int[][]
-        {
-            new int[] {0, 2, 0},
+        //MatrixOrient matrixOrient = new MatrixOrient();
+        //matrixOrient.matrix(new int[][]
+        //{
+        //    new int[] {0, 2, 0},
+        //    new int[] {4, 5, 6},
+        //    new int[] {7, 0, 9}
+        //});
+
+        MatrixSum matrixSum = new MatrixSum();
+        int[] result = matrixSum.MatrixMaxSum(new int[][]
+            {
+            new int[] {1, 2, 3},
             new int[] {4, 5, 6},
-            new int[] {7, 0, 9}
+            new int[] {7, 8, 9}
         });
+        Console.WriteLine("Row: " + result[0] + ", Max Sum: " + result[1]);
     }
 }
