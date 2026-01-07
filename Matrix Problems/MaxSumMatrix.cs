89f3d2cadd570883252f1fc3b21c0682c880cf7f
Redi <redicurrii@yahoo.com>
2026-01-07 08:36:44 +0100

    On this folder I'll do some matrix problems

---
+++ Matrix Problems/MaxSumMatrix.cs
@@ -0,0 +1,22 @@
+﻿public class MatrixSum
+{
+    public int[] MatrixMaxSum(int[][] A)
+    {
+        int row=A.Length;
+        int cols=A[0].Length;
+        int maxSum=int.MinValue;
+        int[] result=new int[2];
+        for(int i=0;i<row;i++){
+            int currentSum=0;
+            for(int j=0;j<cols;j++){
+                currentSum+=A[i][j];
+            }
+            if(currentSum>maxSum){
+                maxSum=currentSum;
+                result[0]=i;
+                result[1]=maxSum;
+            }
+        }
+        return result;
+    }
+}
