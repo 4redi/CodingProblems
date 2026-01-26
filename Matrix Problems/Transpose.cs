e4020f4f6a6fcd86b64028a7b98f9e50edade2ee
Redi <redicurrii@yahoo.com>
2026-01-26 19:31:28 +0100

    Did the transpose of a matrix
    used a 2d array, not a jagged one

---
+++ Matrix Problems/Transpose.cs
@@ -0,0 +1,24 @@
+﻿public class Transpose
+{
+    public void TransposeMe(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        int[,] transposed = new int[cols, rows];
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                transposed[j,i]= matrix[i,j];
+            }
+        }
+
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                System.Console.Write(transposed[i,j] + " ");
+            }
+        }
+    }
+}
