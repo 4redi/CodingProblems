bb4645ec96d044cfb47e69f05bdbed03f096784c
Redi <redicurrii@yahoo.com>
2026-03-16 16:19:59 +0100

    Created a new folder for random problems that come into my head

---
+++ Random/IsPrime.cs
@@ -0,0 +1,19 @@
+﻿class isPrime
+{
+    public void findIt(int num)
+    {
+        if (num <= 1)
+        {
+            Console.WriteLine("Not Prime");
+            return;
+        }
+        for(int i = 2; i <= Math.Sqrt(num); i++)
+        {
+                        if(num % i == 0) { 
+                Console.WriteLine("Not Prime");
+                return;
+            }
+        }
+        Console.WriteLine("Prime");
+    }
+}
