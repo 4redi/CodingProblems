e51f596434a4d99ada231a427a4aece84157f7dc
Redi <redicurrii@yahoo.com>
2026-03-16 16:20:26 +0100

    Testet my program : Finding if a number is prime or not

--- Program.cs
+++ Program.cs
@@ -3,11 +3,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var res = OrderFHalf.Order([2,7,3,6,7,2]);
-        foreach(var i in res)
-        {
-            Console.WriteLine(string.Join(", ", i));
-        }
+        isPrime prime = new isPrime();
+        prime.findIt(6);
     }
 }
 
