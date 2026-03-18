7051fae00a0e4a93d93ec7cf46ff02dae6ebe7e7
Redi <redicurrii@yahoo.com>
2026-03-18 16:56:31 +0100

    Tested 2dPrefixSum

--- Program.cs
+++ Program.cs
@@ -3,8 +3,24 @@ class Program
 {
     public static void Main(string[] args)
     {
-        isPrime prime = new isPrime();
-        prime.findIt(6);
+        _2dPrefixSum prefixSum = new _2dPrefixSum();
+        int[][] grid = new int[][]
+        {
+            new int[] {1, 2, 3},
+            new int[] {4, 5, 6},
+            new int[] {7, 8, 9}
+        };
+        int[,] result = prefixSum._2dSum(grid);
+        int m = grid.Length;
+        int n= grid[0].Length;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
 
