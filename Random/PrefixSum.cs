ef863bf69fb8db6d05fb7ee44e0af1f7ab343724
Redi <redicurrii@yahoo.com>
2026-03-18 16:54:07 +0100

    Approach for 1d arrays
    General formula: arr[i]=arr[i-1]+arr[i]

---
+++ Random/PrefixSum.cs
@@ -0,0 +1,21 @@
+﻿class prefixSum
+{
+    public int[] PrefixSum(int[] arr)
+    {
+        /* Psh: {20,10,50}
+         * {20} iterimi 1
+         * {20,30} iterimi 2
+         * {20,30,80} iterimi 3
+         */
+        int n =arr.Length;
+        int[] prefixSumArr = new int[n];
+        prefixSumArr[0] = arr[0];
+        for(int i=1; i<n; i++)
+        {
+            prefixSumArr[i] = prefixSumArr[i-1] + arr[i];
+
+        }
+
+        return prefixSumArr;
+    }
+}
