d4ae5119ca82ffefa7f9f3ddc5d2686f7854b6d9
Redi <redicurrii@yahoo.com>
2026-03-19 19:36:09 +0100

    Count Submatrices with Top-Left Element and Sum Less Than k
    Nice prefix sum problem

---
+++ Leetcode/LeetCode3070.cs
@@ -0,0 +1,18 @@
+﻿public class Problem3070
+{
+    public int CountSubmatrices(int[][] grid, int k)
+    {
+        int m = grid.Length, n = grid[0].Length;
+        int ans = 0;
+        int[,] ps = new int[m + 1, n + 1];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                ps[i + 1, j + 1] = grid[i][j] + ps[i, j + 1] + ps[i + 1, j] - ps[i, j];
+                if (ps[i + 1, j + 1] <= k) ans++;
+            }
+        }
+        return ans;
+    }
+}
