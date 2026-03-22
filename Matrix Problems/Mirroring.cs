1e0821f81665c2e5dd0f513d79d9f243b959c96a
Redi <redicurrii@yahoo.com>
2026-03-22 12:02:50 +0100

    Added a new problem, to mirror elements of a matrix

---
+++ Matrix Problems/Mirroring.cs
@@ -0,0 +1,17 @@
+﻿class Mirroring
+{
+    public static void MirrorMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols / 2; j++)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, cols - 1 - j];
+                matrix[i, cols - 1 - j] = temp;
+            }
+        }
+    }
+}
