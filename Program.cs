59c0fed8f8c156a3ef2c83a7a4ce1d1670093f78
Redi <redicurrii@yahoo.com>
2026-04-11 20:27:16 +0200

    Add changes

--- Program.cs
+++ Program.cs
@@ -3,24 +3,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        _2dPrefixSum prefixSum = new _2dPrefixSum();
-        int[][] grid = new int[][]
-        {
-            new int[] {1, 2, 3},
-            new int[] {4, 5, 6},
-            new int[] {7, 8, 9}
-        };
-        int[,] result = prefixSum._2dSum(grid);
-        int m = grid.Length;
-        int n= grid[0].Length;
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                Console.Write(result[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
+        Leetcode3741 leet = new Leetcode3741();
+        Console.WriteLine(leet.MinimumDistance(new int[] { 1, 2, 1, 1, 3 }));
     }
 }
 
