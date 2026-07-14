b6ca00daf12abefe3db45efd3a0d7a236585a0fd
Redi <redicurrii@yahoo.com>
2026-07-14 17:56:05 +0200

    Med leetcode problem, to remove nth node from linked list

---
+++ Leetcode/Leetcode19.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Leetcode19
+{
+	public ListNode RemoveNthFromEnd(ListNode head, int n)
+	{
+		ListNode fast = head; ListNode slow = head;
+		for (int i = 0; i < n; i++)
+		{
+			if (fast == null) return head;
+			fast = fast.next;
+		}
+
+		if (fast == null)
+		{
+			return head.next;
+		}
+
+		while (fast.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next;
+		}
+		slow.next = slow.next.next;
+		return head;
+	}
+}
