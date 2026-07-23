26ced80f5fd624fa003b6f6bd9fbe16e92cb468e
Redi <redicurrii@yahoo.com>
2026-07-23 13:37:58 +0200

    Fixes

--- Testing.UnitTests/Leetcode19Tests.cs
+++ Testing.UnitTests/Leetcode19Tests.cs
@@ -8,7 +8,7 @@ namespace Leetcode19.Tests
         [Trait("Category", "LeetCode19")]
         public void Test1()
         {
-            var solution = new Leetcode19();
+            var solution = new Leetcode19Solution();
             var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
             int n = 2;
             var result = solution.RemoveNthFromEnd(head, n);
@@ -18,7 +18,7 @@ namespace Leetcode19.Tests
         [Trait("Category", "LeetCode19")]
         public void Test2()
         {
-            var solution = new Leetcode19();
+            var solution = new Leetcode19Solution();
             var head = new ListNode(1);
             int n = 1;
             var result = solution.RemoveNthFromEnd(head, n);
@@ -28,7 +28,7 @@ namespace Leetcode19.Tests
         [Trait("Category", "LeetCode19")]
         public void Test3()
         {
-            var solution = new Leetcode19();
+            var solution = new Leetcode19Solution();
             var head = new ListNode(1, new ListNode(2));
             int n = 1;
             var result = solution.RemoveNthFromEnd(head, n);
